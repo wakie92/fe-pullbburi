8fb6189a72af048fb4ff7eb66ccc12a45e8584a0
Oscar-bbuilders <oscar@bbuilders.xyz>
2024-07-02 14:21:56 +0900

    chore: 초기 환경 셋팅

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+      },
+    ],
+    'babel-plugin-styled-components',
+  ],
 };
